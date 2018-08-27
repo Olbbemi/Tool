d9c70cdb50c1797743d1eed47cd0f5f3d581e5f3
Olbbemi <shaiya1@naver.com>
2018-08-28 01:22:20 +0900

    2018-08-28

--- C++_Library/Profile/Profile.cpp
+++ C++_Library/Profile/Profile.cpp
@@ -1,29 +1,35 @@
 #include "Precompile.h"
 #include "Profile.h"
 
+#include "Log/Log.h"
+
 #include <time.h>
 #include <stdlib.h>
 #include <strsafe.h>
 
 using namespace Olbbemi;
 
-C_Profile::C_Node::C_Node(const PTCHAR pa_str, __int64 pa_time)
-{
-	m_min_count = m_max_count = 0;
-	m_call_count = m_total_time = 0;
-	m_start_time = pa_time;
-	StringCchCopy(m_function_name, _countof(m_function_name), pa_str);
+LONG C_Profile::s_index = 0;
+DWORD C_Profile_Chunk::s_tls_index = 0;
+C_Profile_Chunk** C_Profile::s_chunk = nullptr;
 
-	for (int i = 0; i < 3; i++)
-	{
-		m_max_value[i] = 0;
-		m_min_value[i] = 0x7FFFFFFFFFFFFFFF;
-	}
+C_Profile::C_Profile(int pa_thread_count) : m_is_lock_on(true), m_file(_TEXT("Profile_"))
+{
+	s_index = -1;	m_thread_count = pa_thread_count;
+	QueryPerformanceFrequency(&m_frequency);
+
+	s_chunk = new C_Profile_Chunk*[pa_thread_count];
+
+	for (int i = 0; i < pa_thread_count; i++)
+		s_chunk[i] = new C_Profile_Chunk;
 }
 
-C_Profile::C_Profile() : m_is_lock_on(true), m_file(_TEXT("Profile_"))
+C_Profile::~C_Profile()
 {
-	QueryPerformanceFrequency(&m_frequency);
+	for (int i = 0; i < m_thread_count; i++)
+		delete s_chunk[i];
+
+	delete s_chunk;
 }
 
 void C_Profile::M_GetTime()
@@ -39,51 +45,173 @@ void C_Profile::M_GetTime()
 	StringCchCat(m_file, _countof(m_file), lo_tail);
 }
 
-void Olbbemi::Profile_Begin(C_Profile *pa_obj, const PTCHAR pa_str, int pa_line)
+void C_Profile::M_Save()
 {
-	C_Profile::E_Status lo_flag = C_Profile::E_Status::normal;
-	for (auto p = pa_obj->m_node_list.begin(); p != pa_obj->m_node_list.end(); p++)
+	FILE *lo_output = nullptr;
+
+	if (GetAsyncKeyState('L') & 0x0001)
+	{
+		m_is_lock_on = true;
+		_tprintf(_TEXT("-----L O C K-----\n"));
+	}
+	else if (GetAsyncKeyState('U') & 0x0001)
+	{
+		m_is_lock_on = false;
+		_tprintf(_TEXT("-----U N L O C K-----\n"));
+	}
+	else if (m_is_lock_on == false && (GetAsyncKeyState(VK_HOME) & 0x0001))
+	{
+		int lo_thread_count = m_thread_count;
+		M_GetTime();
+
+		_tfopen_s(&lo_output, m_file, _TEXT("w, ccs=UTF-16LE"));
+
+		for (int i = 0; i < lo_thread_count; i++)
+		{
+			_ftprintf_s(lo_output, _TEXT("  -----------------------------------------------------------------------------------------------------------------------\n"));
+			_ftprintf_s(lo_output, _TEXT("#     ThreadID    #            Name            #       Average       #       Min_Value       #       Max_Value       #       Count       #\n"));
+
+			for (auto p = s_chunk[i]->m_node_list.begin(); p != s_chunk[i]->m_node_list.end(); p++)
+			{
+				__int64 MaxValue = (*p)->m_max_value[0], MinValue = (*p)->m_min_value[0];
+
+				for (int i = 1; i < (*p)->m_max_count; i++)
+				{
+					if ((*p)->m_max_value[i] > MaxValue)
+					{
+						(*p)->m_total_time -= MaxValue;
+						MaxValue = (*p)->m_max_value[i];
+					}
+					else
+						(*p)->m_total_time -= (*p)->m_max_value[i];
+				}
+				(*p)->m_call_count -= ((*p)->m_max_count - 1);
+
+				for (int i = 1; i < (*p)->m_min_count; i++)
+				{
+					if ((*p)->m_min_value[i] < MinValue)
+					{
+						(*p)->m_total_time -= MinValue;
+						MinValue = (*p)->m_min_value[i];
+					}
+					else
+						(*p)->m_total_time -= (*p)->m_min_value[i];
+				}
+				(*p)->m_call_count -= ((*p)->m_min_count - 1);
+
+				_ftprintf_s(lo_output, _TEXT("#%d#%30s#%22.4Lf#%24.4Lf#%24.4Lf#%18lld#\n"), s_chunk[i]->m_thread_id, (*p)->m_function_name, ((long double)(*p)->m_total_time / (long double)(*p)->m_call_count / (long double)m_frequency.QuadPart * MICRO), ((long double)MinValue / (long double)m_frequency.QuadPart * MICRO), ((long double)MaxValue / (long double)m_frequency.QuadPart * MICRO), (*p)->m_call_count);
+			}
+
+			_ftprintf_s(lo_output, _TEXT("  -----------------------------------------------------------------------------------------------------------------------\n"));
+		}
+
+		fclose(lo_output);
+		m_file[0] = '\0';
+	}
+}
+
+C_Profile_Chunk* C_Profile::S_Alloc()
+{
+	LONG lo_index = InterlockedIncrement(&s_index);
+	return s_chunk[lo_index];
+}
+
+C_Profile_Chunk::C_Node::C_Node(const PTCHAR pa_str, __int64 pa_time)
+{
+	m_min_count = m_max_count = 0;
+	m_call_count = m_total_time = 0;
+	m_start_time = pa_time;
+	StringCchCopy(m_function_name, _countof(m_function_name), pa_str);
+
+	for (int i = 0; i < 3; i++)
+	{
+		m_max_value[i] = 0;
+		m_min_value[i] = 0x7FFFFFFFFFFFFFFF;
+	}
+}
+
+C_Profile_Chunk::C_Profile_Chunk()
+{
+	s_tls_index = TlsAlloc();
+	if (s_tls_index == TLS_OUT_OF_INDEXES)
+	{
+		TCHAR lo_action[] = _TEXT("Profile"), lo_server[] = _TEXT("NONE");
+		ST_Log lo_log({ "TlsAlloc Error Code: " + to_string(GetLastError()) });
+		_LOG(__LINE__, LOG_LEVEL_SYSTEM, lo_action, lo_server, lo_log.count, lo_log.log_str);
+	}
+}
+
+C_Profile_Chunk::~C_Profile_Chunk()
+{
+	int size = m_node_list.size();
+	for (int i = 0; i < size; i++)
+		delete m_node_list[i];
+
+	m_node_list.clear();
+}
+
+void Olbbemi::Profile_Begin(const PTCHAR pa_str, int pa_line)
+{
+	void* lo_tls_ptr = TlsGetValue(C_Profile_Chunk::s_tls_index);
+	if (lo_tls_ptr == nullptr)
+	{
+		lo_tls_ptr = (C_Profile_Chunk*)C_Profile::S_Alloc();
+		BOOL lo_check = TlsSetValue(C_Profile_Chunk::s_tls_index, lo_tls_ptr);
+		if (lo_check == 0)
+		{
+			TCHAR lo_action[] = _TEXT("Profile"), lo_server[] = _TEXT("NONE");
+			ST_Log lo_log({ "TlsSetValue Error Code: " + to_string(GetLastError()) });
+			_LOG(__LINE__, LOG_LEVEL_SYSTEM, lo_action, lo_server, lo_log.count, lo_log.log_str);
+		}
+
+		((C_Profile_Chunk*)lo_tls_ptr)->m_thread_id = GetCurrentThreadId();
+	}
+
+	C_Profile_Chunk::E_Status lo_flag = C_Profile_Chunk::E_Status::normal;
+	for (auto p = ((C_Profile_Chunk*)lo_tls_ptr)->m_node_list.begin(); p != ((C_Profile_Chunk*)lo_tls_ptr)->m_node_list.end(); p++)
 	{
 		if (!_tcscmp((*p)->m_function_name, pa_str))
 		{
 			if ((*p)->m_start_time != 0)
-				lo_flag = C_Profile::E_Status::fail;
+				lo_flag = C_Profile_Chunk::E_Status::fail;
 			else
 			{
-				lo_flag = C_Profile::E_Status::success;
-				QueryPerformanceCounter(&pa_obj->m_begin_count);
-				(*p)->m_start_time = pa_obj->m_begin_count.QuadPart;
+				lo_flag = C_Profile_Chunk::E_Status::success;
+				QueryPerformanceCounter(&((C_Profile_Chunk*)lo_tls_ptr)->m_begin_count);
+				(*p)->m_start_time = ((C_Profile_Chunk*)lo_tls_ptr)->m_begin_count.QuadPart;
 			}
 
 			break;
 		}
 	}
 
-	if (lo_flag == C_Profile::E_Status::fail)
+	if (lo_flag == C_Profile_Chunk::E_Status::fail)
 	{
 		_tprintf(_TEXT("%d : Need to End Function"), pa_line);
 		throw;
 	}
-	else if (lo_flag == C_Profile::E_Status::normal)
+	else if (lo_flag == C_Profile_Chunk::E_Status::normal)
 	{
-		QueryPerformanceCounter(&pa_obj->m_begin_count);
-		C_Profile::C_Node *lo_new_node = new C_Profile::C_Node(pa_str, pa_obj->m_begin_count.QuadPart);
-		pa_obj->m_node_list.push_back(lo_new_node);
+		QueryPerformanceCounter(&((C_Profile_Chunk*)lo_tls_ptr)->m_begin_count);
+		C_Profile_Chunk::C_Node *lo_new_node = new C_Profile_Chunk::C_Node(pa_str, ((C_Profile_Chunk*)lo_tls_ptr)->m_begin_count.QuadPart);
+		((C_Profile_Chunk*)lo_tls_ptr)->m_node_list.push_back(lo_new_node);
 	}
 }
 
-void Olbbemi::Profile_End(C_Profile *pa_obj, const PTCHAR pa_str)
+void Olbbemi::Profile_End(const PTCHAR pa_str)
 {
-	QueryPerformanceCounter(&pa_obj->m_end_count);
-	for (auto p = pa_obj->m_node_list.begin(); p != pa_obj->m_node_list.end(); p++)
+	void* lo_tls_ptr = TlsGetValue(C_Profile_Chunk::s_tls_index);
+
+	QueryPerformanceCounter(&((C_Profile_Chunk*)lo_tls_ptr)->m_end_count);
+	for (auto p = ((C_Profile_Chunk*)lo_tls_ptr)->m_node_list.begin(); p != ((C_Profile_Chunk*)lo_tls_ptr)->m_node_list.end(); p++)
 	{
 		if (!_tcscmp((*p)->m_function_name, pa_str))
 		{
 			(*p)->m_call_count++;
-			(*p)->m_total_time += pa_obj->m_end_count.QuadPart - (*p)->m_start_time;
+			(*p)->m_total_time += ((C_Profile_Chunk*)lo_tls_ptr)->m_end_count.QuadPart - (*p)->m_start_time;
 
-			if ((*p)->m_min_count != 3 && (*p)->m_min_value[(*p)->m_min_count] > pa_obj->m_end_count.QuadPart - (*p)->m_start_time)
-				(*p)->m_min_value[(*p)->m_min_count++] = pa_obj->m_end_count.QuadPart - (*p)->m_start_time;
+			if ((*p)->m_min_count != 3 && (*p)->m_min_value[(*p)->m_min_count] > ((C_Profile_Chunk*)lo_tls_ptr)->m_end_count.QuadPart - (*p)->m_start_time)
+				(*p)->m_min_value[(*p)->m_min_count++] = ((C_Profile_Chunk*)lo_tls_ptr)->m_end_count.QuadPart - (*p)->m_start_time;
 			else if ((*p)->m_min_count == 3)
 			{
 				int lo_index = 0;
@@ -97,12 +225,12 @@ void Olbbemi::Profile_End(C_Profile *pa_obj, const PTCHAR pa_str)
 					}
 				}
 
-				if (pa_obj->m_end_count.QuadPart - (*p)->m_start_time < comp_value)
-					(*p)->m_min_value[lo_index] = pa_obj->m_end_count.QuadPart - (*p)->m_start_time;
+				if (((C_Profile_Chunk*)lo_tls_ptr)->m_end_count.QuadPart - (*p)->m_start_time < comp_value)
+					(*p)->m_min_value[lo_index] = ((C_Profile_Chunk*)lo_tls_ptr)->m_end_count.QuadPart - (*p)->m_start_time;
 			}
 
-			if ((*p)->m_max_count != 3 && (*p)->m_max_value[(*p)->m_max_count] < pa_obj->m_end_count.QuadPart - (*p)->m_start_time)
-				(*p)->m_max_value[(*p)->m_max_count++] = pa_obj->m_end_count.QuadPart - (*p)->m_start_time;
+			if ((*p)->m_max_count != 3 && (*p)->m_max_value[(*p)->m_max_count] < ((C_Profile_Chunk*)lo_tls_ptr)->m_end_count.QuadPart - (*p)->m_start_time)
+				(*p)->m_max_value[(*p)->m_max_count++] = ((C_Profile_Chunk*)lo_tls_ptr)->m_end_count.QuadPart - (*p)->m_start_time;
 			else if ((*p)->m_max_count == 3)
 			{
 				int lo_index = 0;
@@ -116,79 +244,12 @@ void Olbbemi::Profile_End(C_Profile *pa_obj, const PTCHAR pa_str)
 					}
 				}
 
-				if (pa_obj->m_end_count.QuadPart - (*p)->m_start_time > comp_value)
-					(*p)->m_max_value[lo_index] = pa_obj->m_end_count.QuadPart - (*p)->m_start_time;
+				if (((C_Profile_Chunk*)lo_tls_ptr)->m_end_count.QuadPart - (*p)->m_start_time > comp_value)
+					(*p)->m_max_value[lo_index] = ((C_Profile_Chunk*)lo_tls_ptr)->m_end_count.QuadPart - (*p)->m_start_time;
 			}
 
 			(*p)->m_start_time = 0;
 			break;
 		}
 	}
-}
-
-void C_Profile::M_Save()
-{
-	FILE *lo_output;
-
-	if (m_is_lock_on == false && (GetAsyncKeyState(VK_HOME) & 0x0001))
-	{
-		C_Profile::M_GetTime();
-
-		_tfopen_s(&lo_output, m_file, _TEXT("w, ccs=UTF-16LE"));
-		_ftprintf_s(lo_output, _TEXT("  -----------------------------------------------------------------------------------------------------------------------\n"));
-		_ftprintf_s(lo_output, _TEXT("#            Name            #       Average       #       Min_Value       #       Max_Value       #       Count       #\n"));
-
-		for (auto p = m_node_list.begin(); p != m_node_list.end(); p++)
-		{
-			__int64 MaxValue = (*p)->m_max_value[0], MinValue = (*p)->m_min_value[0];
-
-			for (int i = 1; i < (*p)->m_max_count; i++)
-			{
-				if ((*p)->m_max_value[i] > MaxValue)
-				{
-					(*p)->m_total_time -= MaxValue;
-					MaxValue = (*p)->m_max_value[i];
-				}
-				else
-					(*p)->m_total_time -= (*p)->m_max_value[i];
-			}
-			(*p)->m_call_count -= ((*p)->m_max_count - 1);
-
-			for (int i = 1; i < (*p)->m_min_count; i++)
-			{
-				if ((*p)->m_min_value[i] < MinValue)
-				{
-					(*p)->m_total_time -= MinValue;
-					MinValue = (*p)->m_min_value[i];
-				}
-				else
-					(*p)->m_total_time -= (*p)->m_min_value[i];
-			}
-			(*p)->m_call_count -= ((*p)->m_min_count - 1);
-
-			_ftprintf_s(lo_output, _TEXT("#%30s#%22.4Lf#%24.4Lf#%24.4Lf#%18lld#\n"), (*p)->m_function_name, ((long double)(*p)->m_total_time / (long double)(*p)->m_call_count / (long double)m_frequency.QuadPart * MICRO), ((long double)MinValue / (long double)m_frequency.QuadPart * MICRO), ((long double)MaxValue / (long double)m_frequency.QuadPart * MICRO), (*p)->m_call_count);
-		}
-
-		_ftprintf_s(lo_output, _TEXT("  -----------------------------------------------------------------------------------------------------------------------\n"));
-		fclose(lo_output);
-
-		m_file[0] = '\0';
-	}
-}
-
-void C_Profile::M_Delete()
-{
-	if (GetAsyncKeyState('L') & 0x0001)
-	{
-		m_is_lock_on = true;
-		_tprintf(_TEXT("-----L O C K-----\n"));
-	}
-	else if (GetAsyncKeyState('U') & 0x0001)
-	{
-		m_is_lock_on = false;
-		_tprintf(_TEXT("-----U N L O C K-----\n"));
-	}
-
-	if (m_is_lock_on == false && (GetAsyncKeyState(VK_END) & 0x0001))
-		m_node_list.clear();
 }
--- C++_Library/Profile/Profile.h
+++ C++_Library/Profile/Profile.h
@@ -1,29 +1,27 @@
 #ifndef Profile
 #define Profile
 #define MICRO 1000000.0
-#define Profiler
 
-#ifdef Profiler
-	#define BEGIN(Obj, Name, LINE)	Profile_Begin(Obj, Name, LINE)
-	#define END(Obj, Name)			Profile_End(Obj, Name)
-#else
-	#define BEGIN(None)
-	#define END(None)
-#endif
+#include <Windows.h>
+#include <tchar.h>
 
 #include <vector>
 using namespace std;
 
 namespace Olbbemi
 {
-	/*
-	 *  프로파일링이 필요한 위치에서 C_Profile 객체 생성 후 BEGIN 및 END 매크로를 이용 [ BEGIN 과 END 매크로는 한 쌍으로 구성 ]
-	 *  M_Save() 함수: 프로파일링된 정보를 파일로 저장하는 함수 [ Home Key ]
-	 *  M_Delete() 함수: 현재까지 프로파일링된 정보를 삭제하는 함수 [ End Key ]
-	 *  U Key: 키 입력 활성화, L Key: 키 입력 비활성화
-	 */
+	#define Profiler
+	#ifdef Profiler
+		#define BEGIN(Name, LINE)	Profile_Begin(Name, LINE)
+		#define END(Name)			Profile_End(Name)
+	#else
+		#define BEGIN(None)
+		#define END(None)
+	#endif
 
-	class C_Profile
+	class C_Profile;
+
+	class C_Profile_Chunk
 	{
 	private:
 		class C_Node
@@ -31,14 +29,16 @@ namespace Olbbemi
 			TCHAR m_function_name[25];
 			int m_min_count, m_max_count;
 
-			_int64 m_min_value[3], m_max_value[3], m_call_count, m_start_time, m_total_time;
+			__int64 m_min_value[3], m_max_value[3], m_call_count, m_start_time, m_total_time;
 
 		public:
 			C_Node(const PTCHAR pa_str, __int64 pa_time);
 
 			friend class C_Profile;
-			friend void Profile_Begin(C_Profile *pa_obj, const PTCHAR pa_str, int pa_line);
-			friend void Profile_End(C_Profile *pa_obj, const PTCHAR pa_str);
+			friend class C_Profile_Chunk;
+			
+			friend void Profile_Begin(const PTCHAR pa_str, int pa_line);
+			friend void Profile_End(const PTCHAR pa_str);
 		};
 
 		enum class E_Status
@@ -48,23 +48,48 @@ namespace Olbbemi
 			success
 		};
 
-		bool m_is_lock_on;
-		TCHAR m_file[30];
-		LARGE_INTEGER m_frequency, m_begin_count, m_end_count;
-
+		DWORD m_thread_id;
+		LARGE_INTEGER m_begin_count, m_end_count;
 		vector<C_Node*> m_node_list;
 
-		void M_GetTime();
+		static DWORD s_tls_index;
+
+		friend class C_Profile;
 
 	public:
-		C_Profile();
-		void M_Save();
-		void M_Delete();
 
-		friend void Profile_Begin(C_Profile *p_obj, const PTCHAR p_str, int p_line);
-		friend void Profile_End(C_Profile *p_obj, const PTCHAR p_str);
+		C_Profile_Chunk();
+		~C_Profile_Chunk();
+		
+		friend void Profile_Begin(const PTCHAR p_str, int p_line);
+		friend void Profile_End(const PTCHAR p_str);
 	};
-}
 
+	class C_Profile
+	{
+	private:
+		bool m_is_lock_on;
+		TCHAR m_file[30];
+		LONG m_thread_count;
+		LARGE_INTEGER m_frequency;
+
+		static LONG s_index;
+		static C_Profile_Chunk** s_chunk; 
+
+		friend class C_Profile_Chunk;
+
+	public:
+		C_Profile(int pa_thread_count);
+		~C_Profile();
+
+		void M_Save();
+		void M_GetTime();
+
+		static C_Profile_Chunk* S_Alloc();
+	};
+
+	void Profile_Begin(const PTCHAR pa_str, int pa_line);
+	void Profile_End(const PTCHAR pa_str);
+}
 
 #endif
